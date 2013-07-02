f1b7f86a0804544003a7b577d6f9be6ba5206869
Steven Senkus <steven.senkus@yahoo.com>
2013-07-01 21:40:55 -0700

    Works better with my configuration!

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-
-I replace the original file found in C:\xampp\htdocs to this one.
-I don't really like the redirect to the /xampp directory; I want to see MY projects.
-This code is easy to add extra directories.  
-I have just included the two that I use XAMPP with.
-
-*/
-
-function getFolders($target_folder) {
+function getFolders($target_folder, $extra_path) {
 	$root_folder = $target_folder;
 	$list_dir = '';
 	$folder_dir = "\n";
@@ -24,14 +15,14 @@ function getFolders($target_folder) {
 	   $dir = $root_folder . "/" . $namearq;
 	   
 	   if(is_dir($dir)){
-		  $folder_dir .= "\t\t\t\t<li><img src=\"folder.png\" width=\"18\" height=\"15\" alt=\"\"><a href=\"/$namearq\" target=\"top\">$namearq<br></a></li>\n";
+		  $folder_dir .= "\t\t\t\t<li><img src=\"folder.png\" width=\"18\" height=\"15\" alt=\"\"><a href=" . $extra_path . "/" . $namearq . 'target="_blank">' .  "/" . $namearq . "<br></a></li>\n";
 	   } 
 	}
 	return $folder_dir;
 }
 
-$list_github_folders = getFolders('C:/Users/steveo/Documents/GitHub');
-$list_htdocs_folders = getFolders('C:/xampp/htdocs');
+$list_github_folders = getFolders('C:/Users/steveo/Documents/GitHub', 'github');
+$list_htdocs_folders = getFolders('C:/xampp/htdocs', '');
 
 echo <<<EOT
 <!DOCTYPE html>
